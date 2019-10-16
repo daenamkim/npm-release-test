554189c6d97146743d874148ee95944c9cc851e7
Daenam Kim <daenam.kim@gmail.com>
2019-10-16 19:08:54 +0900

    Fix typo

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 
 const expect = require('chai').expect;
 const index = require('../dist/index.js');
-describe('simple vlidate test', () => {
+describe('simple validate test', () => {
   it('should return true for null string', () => {
     const result = index.IsNullOrEmpty(null);
     expect(result).to.equal(true);
